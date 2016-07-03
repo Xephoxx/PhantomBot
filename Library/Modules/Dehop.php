9897e49b523a9e409f24935a090555db49614e5e
ManicSec <root@fbi.gov>
2016-07-03 19:25:03 +0100

    Init

---
+++ Library/Modules/Dehop.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules;
+
+class Dehop extends \Core\ModuleBase
+{
+	public $helpline = 'half ops the given nickname, only usable by +o and above.';
+	
+	public function process(&$that, &$socket, $data, $input, $command, $args)
+	{
+		$sender = $that->sender($data);
+		$channel = $that->channel($data);
+		$input = explode(' ', $that->input($data));
+		unset($input[0]);
+		$input = implode(' ', $input);
+		if($that->getLevel($sender, $channel) > 3 || $that->getLevel($sender, '', $that->host($data)) > 6)
+		{
+			$this->send($socket, 'MODE ' . $channel . ' -h ' . $input);
+			return;
+		}
+		else
+		{
+			$this->privmsg($socket, $channel, "{$sender}: You are not authorized to do that.");
+		}
+	}
+}
