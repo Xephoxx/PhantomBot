0006e690d1fe5ab811e5930a2f660f3b08847312
ManicSec <root@fbi.gov>
2016-07-03 19:22:01 +0100

    Init

---
+++ Library/Modules/Voice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules;
+
+class Voice extends \Core\ModuleBase
+{
+	public $helpline = 'voices the given nickname, only usable by +h and above.';
+	
+	public function process(&$that, &$socket, $data, $input, $command, $args)
+	{
+		$sender = $that->sender($data);
+		$channel = $that->channel($data);
+		$input = explode(' ', $that->input($data));
+		unset($input[0]);
+		$input = implode(' ', $input);
+		if($that->getLevel($sender, $channel) > 2 || $that->getLevel($sender, '', $that->host($data)) > 6)
+		{
+			$this->send($socket, 'MODE ' . $channel . ' +v ' . $input);
+			return;
+		}
+		else
+		{
+			$this->privmsg($socket, $channel, "{$sender}: You are not authorized to do that.");
+		}
+	}
+}
