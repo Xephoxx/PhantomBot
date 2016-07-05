da120c98475bbe25bef3859a9a2acd87404c4674
ManicSec <root@fbi.gov>
2016-07-05 20:05:22 +0100

    Init

---
+++ Library/Modules/Pyeval.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Modules;
+
+class Pyeval extends \Core\ModuleBase
+{
+	public $helpline = 'evals Python code from given input!';
+	
+	public $minAcl = 1;
+	
+	public function process(&$that, &$socket, $data, $input, $command, $args)
+	{
+		$sender = $that->sender($data);
+		$channel = $that->channel($data);
+		
+		$input = explode(' ', $that->input($data));
+		unset($input[0]);
+		$input = implode(' ', $input);
+		
+		$input = stripslashes($input);
+		$input = stripcslashes($input);
+		
+		$data = file_get_contents('http://eval.appspot.com/eval?statement=' . urlencode($input));
+
+		$this->privmsg($socket, $channel, '[PY] ' . $data);
+	}
+}
