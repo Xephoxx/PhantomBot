8aab78c6c7389f1b1c1087a2220157c354438a15
ManicSec <root@fbi.gov>
2016-07-06 18:52:37 +0100

    Init

---
+++ Library/Modules/Protect.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules;
+
+class Protect extends \Core\ModuleBase
+{
+	public $helpline = 'ops the given nickname, only usable by +o and above.';
+	
+	public $minAcl = 6;	
+
+	public function process(&$that, &$socket, $data, $input, $command, $args)
+	{
+		$sender = $that->sender($data);
+		$channel = $that->channel($data);
+		$input = explode(' ', $that->input($data));
+		unset($input[0]);
+		$input = implode(' ', $input);
+		if($that->getLevel($sender, $channel) > 5 || $that->getLevel($sender, '', $that->host($data)) > 6)
+		{
+			if($that->getLevel($that->nick, $channel) > 5)
+			{
+				$this->send($socket, 'MODE ' . $channel . ' +a ' . $input);
+				return;
+			}
+			$this->privmsg($socket, $channel, "{$sender}: I am not authorized to do that."));
+			return;
+		}
+		else
+		{
+			$this->privmsg($socket, $channel, "{$sender}: You are not authorized to do that.");
+		}
+	}
+}
